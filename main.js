1feec4d61f1e04cd7ce9902c8e3d46a4bae73155
yonatan astrachan <yonatank96@gmail.com>
2018-08-16 14:09:06 +0300

    finished gallery

--- main.js
+++ main.js
@@ -9,6 +9,27 @@ for(var i = 1;i<6;i++){
   var newImage = document.createElement('img');
   newImage.setAttribute('src', "images/pic"+ i +".jpg");
   thumbBar.appendChild(newImage);
+  newImage.addEventListener('click',imageLoop,false)
+}
+function imageLoop(){
+  var displayMe = this.getAttribute("src");
+  console.log(displayMe);
+  displayedImage.setAttribute("src",displayMe);
 }
 /* Wiring up the Darken/Lighten button */
 
+btn.addEventListener('click',darkenFunc,false);
+function darkenFunc(){
+  var btnClass = this.getAttribute('class');
+  console.log(btnClass);
+  if(btnClass == 'dark'){
+    this.setAttribute('class','light');
+    this.textContent="Lighten";
+    document.getElementsByClassName('overlay')[0].style.backgroundColor = "rgba(0,0,0,0.5)";
+  }
+  else{
+    this.setAttribute('class','dark');
+    this.textContent="Darken"
+    document.getElementsByClassName('overlay')[0].style.backgroundColor = "rgba(0,0,0,0)";    
+  }
+}
